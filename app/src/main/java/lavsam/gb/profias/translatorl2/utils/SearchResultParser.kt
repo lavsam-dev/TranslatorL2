2d7702a61ce93aa3c15ca8ca7642ac2889501966
lavsam-dev <lavsam@gmail.com>
2021-12-05 22:07:00 +0400

    utils

---
+++ app/src/main/java/lavsam/gb/profias/translatorl2/utils/SearchResultParser.kt
@@ -0,0 +1,47 @@
+package lavsam.gb.profias.translatorl2.utils
+
+import lavsam.gb.profias.translatorl2.model.Meanings
+import lavsam.gb.profias.translatorl2.model.Vocabulary
+import lavsam.gb.profias.translatorl2.model.data.AppState
+
+fun parseSearchResults(state: AppState): AppState {
+    val newSearchResults = arrayListOf<Vocabulary>()
+    when (state) {
+        is AppState.Success -> {
+            val searchResults = state.data
+            if (!searchResults.isNullOrEmpty()) {
+                for (searchResult in searchResults) {
+                    parseResult(searchResult, newSearchResults)
+                }
+            }
+        }
+    }
+
+    return AppState.Success(newSearchResults)
+}
+
+private fun parseResult(dataModel: Vocabulary, newDataModels: ArrayList<Vocabulary>) {
+    if (!dataModel.text.isNullOrBlank() && !dataModel.meanings.isNullOrEmpty()) {
+        val newMeanings = arrayListOf<Meanings>()
+        for (meaning in dataModel.meanings) {
+            if (meaning.translation != null && !meaning.translation.translation.isNullOrBlank()) {
+                newMeanings.add(Meanings(meaning.translation, meaning.imageUrl))
+            }
+        }
+        if (newMeanings.isNotEmpty()) {
+            newDataModels.add(Vocabulary(dataModel.text, newMeanings))
+        }
+    }
+}
+
+fun convertMeaningsToString(meanings: List<Meanings>): String {
+    var meaningsSeparatedByComma = String()
+    for ((index, meaning) in meanings.withIndex()) {
+        meaningsSeparatedByComma += if (index + 1 != meanings.size) {
+            String.format("%s%s", meaning.translation?.translation, ", ")
+        } else {
+            meaning.translation?.translation
+        }
+    }
+    return meaningsSeparatedByComma
+}
